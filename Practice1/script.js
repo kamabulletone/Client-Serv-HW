54d8ef397057e3de3b262c6bcbc94c31fd3be3b2
Lvov Tinley <tin-97@yandex.ru>
2020-09-05 20:00:02 +0300

    added js

---
+++ Practice1/script.js
@@ -0,0 +1,8 @@
+alert(location);
+if(location=="file:///D:/Client-Serv-HW-repos/Practice1/Practic1.html"){
+ setTimeout('location.replace("Practic1(for%20js).html")', 3000);
+}
+
+if(location=="file:///D:/Client-Serv-HW-repos/Practice1/Practic1(for%20js).html"){
+  setTimeout('location.replace("Practic1.html")', 3000);
+}
